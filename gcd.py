ea994e66aa2e414469f20388d2c1e764a93bdcc2
DashaMax <be9emotkot@yandex.ru>
2023-02-24 00:58:37 +0300

    update

--- gcd.py
+++ gcd.py
@@ -1,24 +1,25 @@
-def gcd(a, b):
-    if a == b:
-        return a
-    elif a == 0:
-        return b
-    elif b == 0:
-        return a
+# def gcd(a, b):
+#     if a == b:
+#         return a
+#     elif a == 0:
+#         return b
+#     elif b == 0:
+#         return a
+#
+#     if b > a:
+#         a, b = b, a
+#
+#     while b:
+#         a, b = b, a % b
+#
+#     return a
+#
+#
+# def main():
+#     a, b = map(int, input().split())
+#     print(gcd(a, b))
+#
+#
+# if __name__ == "__main__":
+#     main()
 
-    if b > a:
-        a, b = b, a
-
-    while b:
-        a, b = b, a % b
-
-    return a
-
-
-def main():
-    a, b = map(int, input().split())
-    print(gcd(a, b))
-
-
-if __name__ == "__main__":
-    main()
