7518ebb7b18038758484659fca6d74cd281b77db
DashaMax <be9emotkot@yandex.ru>
2023-02-27 01:02:23 +0300

    add

--- divide_and_conquer.py
+++ divide_and_conquer.py
@@ -3,29 +3,73 @@
 #####################
 
 
-def binary_search(l: list, a: int) -> int:
-    left = 0
-    right = len(l)
-    middle = (left + right) // 2
+# def binary_search(l: list, a: int) -> int:
+#     left = 0
+#     right = len(l)
+#     middle = (left + right) // 2
+#
+#     while left <= right and middle < len(l):
+#         if a == l[middle]:
+#             return middle + 1
+#         elif a < l[middle]:
+#             right = middle - 1
+#         else:
+#             left = middle + 1
+#
+#         middle = (left + right) // 2
+#
+#     return -1
+#
+#
+# def main():
+#     A = list(map(int, input('-> ').split()))[1:]
+#     B = list(map(int, input('-> ').split()))[1:]
+#     result = [binary_search(A, x) for x in B]
+#     print(*result)
 
-    while left <= right and middle < len(l):
-        if a == l[middle]:
-            return middle + 1
-        elif a < l[middle]:
-            right = middle - 1
+
+
+
+
+############################
+#   Number of inversions   #
+############################
+
+
+COUNT = 0
+
+
+def get_inversions(l: list):
+    if len(l) == 1:
+        return l
+
+    left = get_inversions(l[:len(l) // 2])
+    right = get_inversions(l[len(l) // 2:])
+    list_sort = []
+    global COUNT
+    i = j = 0
+
+    while i < len(left) and j < len(right):
+        if left[i] > right[j]:
+            COUNT += len(left) - i
+            list_sort.append(right[j])
+            j += 1
         else:
-            left = middle + 1
+            list_sort.append(left[i])
+            i += 1
 
-        middle = (left + right) // 2
+    if i < len(left):
+        list_sort.extend(left[i:])
+    elif j < len(right):
+        list_sort.extend(right[j:])
 
-    return -1
+    return list_sort
 
 
 def main():
-    A = list(map(int, input('-> ').split()))[1:]
-    B = list(map(int, input('-> ').split()))[1:]
-    result = [binary_search(A, x) for x in B]
-    print(*result)
+    numbers = list(map(int, input('-> ').split()))
+    get_inversions(numbers)
+    print(COUNT)
 
 
 if __name__ == '__main__':
